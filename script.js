3d39aa55824352ffa07b26b90e5c77bdd4533248
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-20 12:35:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const currentURL = window.location.origin;
+
+// Get the length of the current URL
+const urlLength = currentURL.length;
+//alert(currentURL)
+// Print the length of the URL using an alert
+alert(`The length of the URL is: ${urlLength}`);
